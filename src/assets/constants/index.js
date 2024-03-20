ea5c851d401d3fd7545e69d3fad61aaa753fe212
IrynaSlavinska <133566139+IrynaSlavinska@users.noreply.github.com>
2024-03-20 23:40:15 +0200

    icons part

---
+++ src/assets/constants/index.js
@@ -0,0 +1,10 @@
+export const VEHICLE_TYPE_ARRAY = [
+  { id: 0, form: 'panelTruck', src: 'icon-van', name: 'Van' },
+  {
+    id: 1,
+    form: 'fullyIntegrated',
+    src: 'icon-fully-integrated',
+    name: 'Fully Integrated',
+  },
+  { id: 2, form: 'alcove', src: 'icon-alcove', name: 'Alcove' },
+];
