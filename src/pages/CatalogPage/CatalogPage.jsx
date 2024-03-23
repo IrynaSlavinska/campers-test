918d438b759d99497641bdfa06a929914fdb8203
IrynaSlavinska <133566139+IrynaSlavinska@users.noreply.github.com>
2024-03-23 20:48:55 +0200

    load more hide

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
 import { getAllAdvert } from '../../redux/advert/operations';
-import { selectIsLoading } from '../../redux/selectors';
-import { selectAdvert } from '../../redux/selectors';
-
+import {
+  selectIsLoading,
+  selectAdvert,
+  selectTotal,
+} from '../../redux/selectors';
 import { Sidebar } from 'components/FilterPartials/Sidebar';
-
 import { CatalogContainer, LoadMoreButton } from './CatalogPage.styled';
 import { Catalog } from 'components/Catalog/Catalog';
 import { Loader } from 'components/Loader/Loader';
@@ -17,6 +17,7 @@ export const CatalogPage = () => {
   const adverts = useSelector(selectAdvert);
   const [page, setPage] = useState(1);
   const loadedBefore = useRef(false);
+  const total = useSelector(selectTotal);
 
   useEffect(() => {
     if (loadedBefore.current) {
@@ -32,21 +33,21 @@ export const CatalogPage = () => {
   };
 
   return (
-    <>
-      <CatalogContainer>
-        <Sidebar />
+    <CatalogContainer>
+      <Sidebar />
 
-        {isLoading ? (
-          <Loader />
-        ) : (
-          <div>
-            <Catalog adverts={adverts} />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <Catalog adverts={adverts} />
+          {adverts.length !== total && (
             <LoadMoreButton type="button" onClick={() => handleMoreClick()}>
               Load more
             </LoadMoreButton>
-          </div>
-        )}
-      </CatalogContainer>
-    </>
+          )}
+        </div>
+      )}
+    </CatalogContainer>
   );
 };
--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,5 +1,4 @@
 export const selectAdvert = state => state.adverts.adverts;
-export const selectAdvertCategories = state => state.adverts.adverts.details;
-export const selectOneAdvert = state => state.adverts.oneAdvert;
 export const selectError = state => state.adverts.error;
 export const selectIsLoading = state => state.adverts.isLoading;
+export const selectTotal = state => state.adverts.totalResults;
